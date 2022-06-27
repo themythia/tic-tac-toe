4c32ade6e8567f4a80c1b25ee26bf76ab8835cc8
Emir Al <mythia@windowslive.com>
2022-06-27 10:34:49 +0300

    feat: add custom styles needed for project to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: '#0026ED',
+        customRed: '#FF0505',
+        customBg: '#F3B18C',
+      },
+      fontFamily: {
+        fingerPaint: ['Finger Paint'],
+      },
+    },
   },
   plugins: [],
 };
