90a87ae9ba0f98905b0ec3f17f522f875aa3052c
Emir Al <mythia@windowslive.com>
2022-06-28 21:02:18 +0300

    fix: styling on smaller screens

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Container from './components/Container';
 
 const App = () => {
   return (
-    <div className='bg-customBg w-screen h-screen flex justify-center items-center'>
+    <div className='bg-customBg w-screen min-h-screen flex justify-center items-center'>
       <Container />
     </div>
   );
